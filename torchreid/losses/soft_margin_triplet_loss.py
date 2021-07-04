39c940ba45b46d5a8221dce1e16fb2f5eb75d42b
Bumble Bee <valuebumblebee@gmail.com>
2021-07-05 02:15:08 +0700

    Update 2 branches

--- torchreid/losses/soft_margin_triplet_loss.py
+++ torchreid/losses/soft_margin_triplet_loss.py
@@ -3,6 +3,7 @@ from __future__ import division
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from .weighted_softmargin_loss import WeightedSoftMarginLoss
 
 
@@ -45,8 +46,10 @@ class TripletLoss(nn.Module):
         mask = targets.expand(n, n).eq(targets.expand(n, n).t())
         dist_ap, dist_an = [], []
         for i in range(n):
-            dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
-            dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+            tmp1 = dist[i][mask[i]]
+            dist_ap.append(torch.sum(F.softmax(tmp1, dim=-1)*tmp1, 0).unsqueeze(0))
+            tmp2 = dist[i][mask[i] == 0]
+            dist_an.append(torch.sum(F.softmax(-tmp2, dim=-1)*tmp2, 0).unsqueeze(0))
         dist_ap = torch.cat(dist_ap)
         dist_an = torch.cat(dist_an)
         
